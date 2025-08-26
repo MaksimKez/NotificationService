182e52846c0fa63f3032503dc69a176836669c98
MaksimKez <prostomaxim765@gmail.com>
2025-08-26 17:59:32 +0200

    EmailSender.cs

--- Api/Program.cs
+++ Api/Program.cs
@@ -2,8 +2,6 @@ using Infrastructure.EmailNotifier.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 
-builder.Services.Configure<EmailSettings>(builder.Configuration.GetSection(EmailSettings.DefaultConfigName));
-builder.Services.Configure<RetryPolicySettings>(builder.Configuration.GetSection(RetryPolicySettings.DefaultConfigName));
 
 
 builder.Services.AddControllers();
