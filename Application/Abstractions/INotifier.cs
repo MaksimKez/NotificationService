67db099d4596307dc49a09df867bfbd5cee294a7
MaksimKez <prostomaxim765@gmail.com>
2025-08-26 12:49:28 +0200

    it can be unclear

--- Application/Abstractions/INotifier.cs
+++ Application/Abstractions/INotifier.cs
@@ -8,5 +8,7 @@ public interface INotifier
     string Name { get; }
     int Priority { get; }
     Task<Result> NotifySingle(UserListingPairDto userListingPair);
+    
+    /// <returns>Result with dictionary, if succeed - dictionary is empty, if not - key is user's id that was not notified and the error why </returns>
     Task<ResultWithClass<Dictionary<Guid, string>>> NotifyMultiple(UserListingPairDto[] userListingPairs);
 }
