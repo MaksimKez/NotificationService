ef50d49a04a0c1a09c5b12685345f8aefa4966dc
MaksimKez <156958833+MaksimKez@users.noreply.github.com>
2025-09-01 15:54:00 +0200

    Tests (#2)
    * perf test with api
    
    * fake email sender
    
    * rate limiting

---
+++ NotificationService.Tests.Perfomance/Program.cs
@@ -0,0 +1,68 @@
+﻿using System.Net.Http.Json;
+using NBomber.CSharp;
+using NotificationService.Tests.Perfomance.Models;
+
+namespace NotificationService.Tests.Perfomance;
+
+class Program
+{
+    static void Main()
+    {
+        var baseUrl = "https://localhost:44388/Listing";
+        var client = new HttpClient();
+
+        var userListing = new UserListingPairDto
+        {
+            User = new UserDto 
+            { 
+                Id = Guid.NewGuid(), 
+                Name = "John", 
+                LastName = "Doe", 
+                Email = "blabla@gmail.com", 
+                TelegramId = null 
+            },
+            Listing = new ListingDto
+            {
+                Id = Guid.NewGuid(),
+                Price = 100000m,
+                AreaMeterSqr = 50,
+                Rooms = 2,
+                Floor = 3,
+                IsFurnished = true,
+                PetsAllowed = false,
+                HasBalcony = true,
+                HasAppliances = true,
+                Url = "https://example.com",
+                CreatedAt = DateTime.UtcNow
+            }
+        };
+
+        var userListings = new[] { userListing, userListing, userListing };
+
+        var singleScenario = Scenario.Create("NotifySingle", async _ =>
+            {
+                var response = await client.PostAsJsonAsync(baseUrl + "/notify-single", userListing);
+                return response.IsSuccessStatusCode 
+                    ? Response.Ok()
+                    : Response.Fail();
+            })
+            .WithLoadSimulations(
+                Simulation.KeepConstant(30, TimeSpan.FromSeconds(10))
+            );
+
+        var multipleScenario = Scenario.Create("NotifyMultiple", async _ =>
+            {
+                var response = await client.PostAsJsonAsync(baseUrl + "/notify-multiple", userListings);
+                return response.IsSuccessStatusCode 
+                    ? Response.Ok()
+                    : Response.Fail();
+            })
+            .WithLoadSimulations(
+                Simulation.KeepConstant(20, TimeSpan.FromSeconds(10))
+            );
+
+        NBomberRunner
+            .RegisterScenarios(singleScenario, multipleScenario)
+            .Run();
+    }
+}
